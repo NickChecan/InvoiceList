7228b184c2b6443dec473aed074ac5f09244f666
NickChecan <nicholas.checan@gmail.com>
2020-03-15 00:30:08 +0000

    Fragment Callbacks

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,28 +1,32 @@
 sap.ui.define([
-   "sap/ui/core/mvc/Controller",
-   "sap/m/MessageToast",
-   "sap/ui/core/Fragment"
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/ui/core/Fragment"
 ], function (Controller, MessageToast, Fragment) {
-   "use strict";
-   return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
-      
-      onShowHello : function () {
-         // Read msg from i18n model
-         var oBundle = this.getView().getModel("i18n").getResourceBundle();
-         var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-         var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-         // Show message
-         MessageToast.show(sMsg);
-      },
-      
-      onOpenDialog : function () {
+	"use strict";
+
+	return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
+
+		onShowHello : function () {
+			// Read msg from i18n model
+			var oBundle = this.getView().getModel("i18n").getResourceBundle();
+			var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+			// Show message
+			MessageToast.show(sMsg);
+		},
+
+		onOpenDialog : function () {
 			var oView = this.getView();
+
 			// Create dialog lazily
 			if (!this.byId("helloDialog")) {
 				// Load asynchronous XML fragment
 				Fragment.load({
 					id: oView.getId(),
-					name: "sap.ui.demo.walkthrough.view.HelloDialog"
+					name: "sap.ui.demo.walkthrough.view.HelloDialog",
+					controller: this
 				}).then(function (oDialog) {
 					// Connect dialog to the root view of this component (models, lifecycle)
 					oView.addDependent(oDialog);
@@ -31,7 +35,12 @@ sap.ui.define([
 			} else {
 				this.byId("helloDialog").open();
 			}
+		},
+
+		onCloseDialog : function () {
+			this.byId("helloDialog").close();
 		}
-      
-   });
+		
+	});
+
 });
