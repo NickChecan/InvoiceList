6f64dbe414f9668b15b7bb1d44a3b053d12d7dd6
NickChecan <nicholas.checan@gmail.com>
2020-03-15 00:01:43 +0000

    Descriptor for Applications

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,20 +1,14 @@
 sap.ui.define([
    "sap/ui/core/UIComponent",
-   "sap/ui/model/json/JSONModel",
-   "sap/ui/model/resource/ResourceModel"
-], function (UIComponent, JSONModel, ResourceModel) {
+   "sap/ui/model/json/JSONModel"
+], function (UIComponent, JSONModel) {
    "use strict";
    return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
    	
       metadata : {
-         rootView: {
-            "viewName": "sap.ui.demo.walkthrough.view.App",
-            "type": "XML",
-            "async": true,
-            "id": "app"
-         }
+            manifest: "json"
       },
-      
+    
       init : function () {
          // Call the init function of the parent
          UIComponent.prototype.init.apply(this, arguments);
@@ -28,12 +22,6 @@ sap.ui.define([
          
          var oModel = new JSONModel(oData);
          this.setModel(oModel);
-
-         // Set i18n model
-         var i18nModel = new ResourceModel({
-            bundleName : "sap.ui.demo.walkthrough.i18n.i18n"
-         });
-         this.setModel(i18nModel, "i18n");
       }
       
    });
