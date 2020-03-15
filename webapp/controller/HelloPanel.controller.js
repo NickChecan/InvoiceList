39126d9e6b6aa136e1197d5876802a538ef2d63d
NickChecan <nicholas.checan@gmail.com>
2020-03-15 02:27:40 +0000

    Reuse Dialogs

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -4,39 +4,23 @@ sap.ui.define([
 	"sap/ui/core/Fragment"
 ], function (Controller, MessageToast, Fragment) {
 	"use strict";
-
+	
 	return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
-
+		
 		onShowHello : function () {
 			// Read msg from i18n model
 			var oBundle = this.getView().getModel("i18n").getResourceBundle();
 			var sRecipient = this.getView().getModel().getProperty("/recipient/name");
 			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-
+			
 			// Show message
 			MessageToast.show(sMsg);
 		},
-
+		
 		onOpenDialog : function () {
-			var oView = this.getView();
-
-			// Create dialog lazily
-			if (!this.byId("helloDialog")) {
-				// Load asynchronous XML fragment
-				Fragment.load({
-					id: oView.getId(),
-					name: "sap.ui.demo.walkthrough.view.HelloDialog",
-					controller: this
-				}).then(function (oDialog) {
-					// Connect dialog to the root view of this component (models, lifecycle)
-					oView.addDependent(oDialog);
-					oDialog.open();
-				});
-			} else {
-				this.byId("helloDialog").open();
-			}
+			this.getOwnerComponent().openHelloDialog();
 		},
-
+		
 		onCloseDialog : function () {
 			this.byId("helloDialog").close();
 		}
